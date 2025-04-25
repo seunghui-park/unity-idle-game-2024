992bc77f84840efb7fa1ebe8ffae76054aa69b8c
박승희 <ridia05031@gmail.com>
2025-04-25 11:04:34 +0900

    [SignInScene] SampleScene으로의 씬 전환 구현

--- Assets/Scripts/changeScene.cs
+++ Assets/Scripts/changeScene.cs
@@ -1,16 +1,10 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class changeScene : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public void OnClickSceneChange()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene("SampleScene");
     }
 }
