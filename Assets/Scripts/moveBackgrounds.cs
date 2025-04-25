49830f0aca8f0780feb05acaf4e92b4b417122de
박승희 <ridia05031@gmail.com>
2025-04-25 10:45:53 +0900

    [SampleScene] 배경 이미지 설정

---
+++ Assets/Scripts/moveBackgrounds.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class moveBackground : MonoBehaviour
+{
+    public float scrollSpeed = 700f;
+    public Image[] backgroundSprite;
+    private float[] backgroundOffset;
+
+    void Start()
+    {
+        backgroundOffset = new float[backgroundSprite.Length];
+        for (int i = 0; i < backgroundSprite.Length; i++)
+            backgroundOffset[i] = backgroundSprite[i].transform.localPosition.x;
+    }
+
+    void Update()
+    {
+        for (int i = 0; i < backgroundSprite.Length; i++)
+        {
+            backgroundOffset[i] -= Time.deltaTime * scrollSpeed;
+            backgroundSprite[i].transform.localPosition = new Vector3(backgroundOffset[i], 0);
+
+            if (backgroundSprite[i].transform.localPosition.x <= -720)
+            {
+                backgroundSprite[i].transform.localPosition = new Vector3(1440, 0);
+                backgroundOffset[i] = 1440;
+            }
+        }
+    }
+
+    public void stopBackground() => scrollSpeed = 0f;
+}
