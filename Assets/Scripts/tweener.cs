1a30ce51f25c490564ad964e0286e4ea007d12b0
박승희 <ridia05031@gmail.com>
2025-05-01 13:02:56 +0900

    feat: SignInScene에 스플래시 이미지 fade 효과 추가

---
+++ Assets/Scripts/tweener.cs
@@ -0,0 +1,18 @@
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class tweener : MonoBehaviour
+{
+    public Image img;
+    void Start()
+    {
+        img.DOFade(0f, 3f).SetEase(Ease.Linear);
+        Invoke("FadeIn", 4f);
+    }
+
+    void FadeIn()
+    {
+        img.DOFade(1f, 3f).SetEase(Ease.Linear);
+    }
+}
